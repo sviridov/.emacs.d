975733331c1b999d6f68751a488819e8a32081dc
Sviridov Alexander <sviridov.vmi@gmail.com>
2013-05-14 10:36:11 +0600

    Added init-base.el and base/init-smex.el

---
+++ base/init-smex.el
@@ -0,0 +1,14 @@
+
+(add-to-load-path "base/smex")
+
+(require 'smex)
+
+(setq-default smex-save-file (concat user-emacs-directory ".smex"))
+
+(smex-initialize)
+
+(global-set-key (kbd "M-x"    ) 'smex                    )
+(global-set-key (kbd "M-X"    ) 'smex-major-mode-commands)
+(global-set-key (kbd "C-c M-x") 'execute-extended-command)
+
+(provide 'init-smex)
---
+++ init-base.el
@@ -0,0 +1,29 @@
+
+(add-to-load-path "base")
+
+(setq-default make-backup-files nil
+	      auto-save-defaults  t
+	      indent-tabs-mode  nil
+              resize-mini-windows t
+              enable-recursive-minibuffers t)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(put 'narrow-to-region 'disabled nil)
+
+(require 'init-smex)
+(require 'init-org)
+(require 'init-ido)
+(require 'init-uniquify)
+(require 'init-russian-computer)
+(require 'init-saveplace)
+(require 'init-magit)
+(require 'init-dired)
+(require 'init-libraries)
+(require 'init-multiple-cursors)
+(require 'init-phi-search)
+(require 'init-ace-jump-mode)
+(require 'init-quick-repl-history-search)
+(require 'init-dictem)
+
+(provide 'init-base)
