c8df05f72de59ed896b88db0f9705ec44caefa13
Sviridov Alexander <sviridov.vmi@gmail.com>
2013-05-14 11:17:19 +0600

    Added base/init-multiple-cursors.el

---
+++ base/init-multiple-cursors.el
@@ -0,0 +1,12 @@
+
+(add-to-load-path "base/multiple-cursors")
+
+(require 'multiple-cursors)
+
+(global-set-key (kbd "S-SPC") 'set-rectangular-region-anchor)
+
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this    )
+(global-set-key (kbd "C-*") 'mc/mark-all-like-this     )
+
+(provide 'init-multiple-cursors)
