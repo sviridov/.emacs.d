68d477efc500b49c968d3975f5d66b57eec2cfc7
Sviridov Alexander <sviridov.vmi@gmail.com>
2013-05-14 11:07:55 +0600

    Added base/init-ace-jump-mode.el

---
+++ base/init-ace-jump-mode.el
@@ -0,0 +1,13 @@
+
+(add-to-load-path "base/ace-jump-mode")
+
+(autoload 'ace-jump-mode "ace-jump-mode" "Emacs quick move minor mode" t)
+(autoload 'ace-jump-mode-pop-mark "ace-jump-mode" "Ace jump back:-)" t)
+
+(eval-after-load "ace-jump-mode"
+  '(ace-jump-mode-enable-mark-sync))
+
+(global-set-key (kbd "C-,") 'ace-jump-mode)
+(global-set-key (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+
+(provide 'init-ace-jump-mode)
