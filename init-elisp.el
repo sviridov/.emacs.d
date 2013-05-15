e114d7002ad6c00bec151f77d32f4e2f2fac8362
Sviridov Alexander <sviridov.vmi@gmail.com>
2013-05-15 15:51:26 +0600

    Added init-elisp.el

---
+++ init-elisp.el
@@ -0,0 +1,24 @@
+
+(add-to-load-path "elisp")
+
+(defun eval-and-replace ()
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
+
+(global-set-key (kbd "C-c C-e") 'eval-and-replace)
+
+(eval-after-load "ielm"
+  '(define-key ielm-map (kbd "C-c C-e")
+     'eval-and-replace))
+
+(add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
+
+(require 'init-elisp-slime-nav)
+(require 'init-litable)
+
+(provide 'init-elisp)
