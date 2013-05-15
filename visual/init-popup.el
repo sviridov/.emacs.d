0e6d3d4bfd8217878a29dac1fb8dd737fc1bf00e
Sviridov Alexander <sviridov.vmi@gmail.com>
2013-05-15 14:46:40 +0600

    Added visual/init-popup.el

---
+++ visual/init-popup.el
@@ -0,0 +1,11 @@
+
+(add-to-load-path "visual/popup")
+
+(require 'popup)
+
+(set-face-attribute 'popup-face nil     :background "Gray10" :foreground "White" )
+(set-face-attribute 'popup-tip-face nil :background "Gray10" :foreground "White" )
+(set-face-attribute 'popup-scroll-bar-background-face nil :background "Gray10"   )
+(set-face-attribute 'popup-scroll-bar-foreground-face nil :background "SteelBlue")
+
+(provide 'init-popup)
