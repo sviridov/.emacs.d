eaa5cf5eb569e5a3d52fabfe5a073d7b5568265e
Alexander Sviridov <sviridov.vmi@gmail.com>
2013-05-17 00:28:53 +0600

    Added visual/init-highlight.el

--- init-visual.el
+++ init-visual.el
@@ -7,18 +7,13 @@
 (require 'init-popup)
 (require 'init-font)
 (require 'init-fullscreen)
+(require 'init-highlight)
 
 (setq-default inhibit-startup-buffer-menu nil
               inhibit-startup-screen        t
               initial-buffer-choice       nil
               initial-scratch-message     nil)
 
-(global-hl-line-mode 1)
-
-(show-paren-mode 1)
-
 (setq scroll-step 1)
-
-(setq-default show-trailing-whitespace t)
 
 (provide 'init-visual)
---
+++ visual/init-highlight.el
@@ -0,0 +1,8 @@
+
+(global-hl-line-mode 1)
+
+(show-paren-mode 1)
+
+(setq-default show-trailing-whitespace t)
+
+(provide 'init-highlight)
