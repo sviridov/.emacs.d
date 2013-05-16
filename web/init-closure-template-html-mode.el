2b4292edd862defe5940c9fe37450f5d56cf0743
Sviridov Alexander <sviridov.vmi@gmail.com>
2013-05-16 12:30:37 +0600

    Added web/init-closure-template-html-mode.el

---
+++ web/init-closure-template-html-mode.el
@@ -0,0 +1,9 @@
+
+(add-to-load-path "web/closure-template-html-mode")
+
+(autoload 'closure-template-html-mode "closure-template-html-mode"
+  "Major mode for editing Closure Template code." t)
+
+(add-to-list 'auto-mode-alist '("\\.tmpl$" . closure-template-html-mode))
+
+(provide 'init-closure-template-html-mode)
