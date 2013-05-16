50c86e5608999da6f913e677e60ad75eb7bb4bbd
Alexander Sviridov <sviridov.vmi@gmail.com>
2013-05-16 20:32:24 +0600

    Added visual/init-fullscreen.el

--- init-visual.el
+++ init-visual.el
@@ -27,16 +27,7 @@
 (require 'init-powerline)
 (require 'init-popup)
 (require 'init-font)
-
-(unless (fboundp 'toggle-fullscreen)
-  (defun toggle-fullscreen ()
-    (interactive)
-    (when (eq window-system 'x)
-      (set-frame-parameter
-       nil 'fullscreen
-       (when (not (frame-parameter nil 'fullscreen)) 'fullboth)))))
-
-(global-set-key [f11] 'toggle-fullscreen)
+(require 'init-fullscreen)
 
 (setq-default show-trailing-whitespace t)
 
