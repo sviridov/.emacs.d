93e289fc0578874153adc8acd20da74ac3ded933
Sviridov Alexander <sviridov.vmi@gmail.com>
2013-05-20 19:07:24 +0600

    Added javascript/init-js2-mode.el

---
+++ javascript/init-js2-mode.el
@@ -0,0 +1,8 @@
+
+(add-to-load-path "javascript/js2-mode")
+
+(autoload 'js2-mode "js2-mode" "Major mode for editing JavaScript code." t)
+
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+(provide 'init-js2-mode)
