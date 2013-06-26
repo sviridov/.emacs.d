8870c026670d6d30be871050f57fc20756940541
Sviridov Alexander <sviridov.vmi@gmail.com>
2013-06-26 18:55:56 +0600

    require scala-mode2 -> autoload

--- init-scala.el
+++ init-scala.el
@@ -2,5 +2,6 @@
 (add-to-load-path "scala")
 
 (require 'init-scala-mode2)
+(require 'init-ensime)
 
 (provide 'init-scala)
--- scala/init-scala-mode2.el
+++ scala/init-scala-mode2.el
@@ -1,13 +1,12 @@
 
 (add-to-load-path "scala/scala-mode2")
 
-(autoload 'scala-mode "scala-mode2" "Major mode for editing Scala code" t)
+(autoload 'scala-mode "scala-mode2" "Scala major mode" t)
 
 (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
 
-(eval-after-load 'scala-mode
- `(progn
-    (setq scala-indent:default-run-on-strategy scala-indent:eager-strategy
-          scala-indent:align-parameters t)))
+(eval-after-load 'scala-mode2
+  `(setq scala-indent:default-run-on-strategy scala-indent:eager-strategy
+         scala-indent:align-parameters t))
 
 (provide 'init-scala-mode2)
