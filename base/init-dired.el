a8d92dc6bd8af5bdf9f0e16939fb0391ec3741d6
Sviridov Alexander <sviridov.vmi@gmail.com>
2013-07-07 18:35:26 +0600

    Modified base/init-dired.el

--- base/init-dired.el
+++ base/init-dired.el
@@ -1,4 +1,13 @@
 
-(setf dired-listing-switches "-alh")
+(require 'dired)
+
+(setq dired-listing-switches "-alh"
+      dired-recursive-copies 'always
+      dired-recursive-deletes 'top
+      dired-dwim-target t)
+
+(global-set-key (kbd "C-x C-j") 'dired-jump)
+
+(put 'dired-find-alternate-file 'disabled nil)
 
 (provide 'init-dired)
