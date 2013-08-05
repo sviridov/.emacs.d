1b72eb561e72bbef90ba514a5be33e4dba3b74fb
Sviridov Alexander <sviridov.vmi@gmail.com>
2013-08-05 13:30:25 +0600

    Refactoring

--- common-lisp/init-common-lisp-slime.el
+++ common-lisp/init-common-lisp-slime.el
@@ -1,36 +1,24 @@
 
-(defun prepare-common-lisp-slime ()
-  (interactive)
+(load "~/quicklisp/slime-helper.el")
 
-  (load "~/quicklisp/slime-helper.el")
+(autoload 'slime "slime" "The Superior Lisp Interaction Mode for Emacs" t)
+(autoload 'slime-connect "slime" "Connect to an already-running `swank' server" t)
 
-  (require 'slime)
+(eval-after-load "slime"
+ '(progn
+    (slime-setup '(slime-repl slime-fancy slime-fuzzy slime-fancy-inspector slime-indentation))
 
-  (slime-setup '(slime-repl slime-fancy slime-fuzzy slime-fancy-inspector slime-indentation))
+    (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol
+          common-lisp-hyperspec-root (concat user-emacs-directory "common-lisp/HyperSpec/"))
 
-  (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol
-	common-lisp-hyperspec-root (concat user-emacs-directory "common-lisp/HyperSpec/"))
+    (define-key slime-mode-map (kbd "TAB")
+      'slime-indent-and-complete-symbol)
 
-  (define-key slime-mode-map (kbd "TAB")
-    'slime-indent-and-complete-symbol)
-
- ; (slime-define-keys slime-repl-mode-map
- ;   ("\C-m"   nil)
- ;   ([return] nil))
-
-  (global-set-key [f12] 'slime-selector)
-
-;  (define-key slime-repl-mode-map (kbd "C-c C-e")
-;    'slime-eval-and-replace)
-
-;  (define-key slime-mode-map (kbd "C-c C-e")
-;    'slime-eval-and-replace)
-  )
+    (global-set-key [f12] 'slime-selector)))
 
 (defmacro defslime-start (name lisp)
  `(defun ,name ()
     (interactive)
-    (prepare-common-lisp-slime)
     (slime ,lisp)))
 
 (setq inferior-lisp-program "sbcl")
