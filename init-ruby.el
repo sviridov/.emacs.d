1648af3cc15daab92425eed9906cb38e5c5da5e9
Sviridov Alexander <sviridov.vmi@gmail.com>
2013-09-18 12:50:51 +0600

    Added rhtml-mode

--- init-ruby.el
+++ init-ruby.el
@@ -2,5 +2,6 @@
 (add-load-path "ruby")
 
 (require 'init-inf-ruby)
+(require 'init-rhtml)
 
 (provide 'init-ruby)
---
+++ ruby/init-rhtml.el
@@ -0,0 +1,9 @@
+
+(add-load-path "ruby/rhtml")
+
+(autoload 'rhtml-mode "rhtml-mode" nil t)
+
+(add-to-list 'auto-mode-alist '("\\.erb\\" . rhtml-mode))
+(add-to-list 'auto-mode-alist '("\\.rjs\\" . rhtml-mode))
+
+(provide 'init-rhtml)
