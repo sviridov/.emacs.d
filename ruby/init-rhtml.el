f9584039d695baa8e1c7905c98e5fb01703638c9
Sviridov Alexander <sviridov.vmi@gmail.com>
2013-09-18 12:56:21 +0600

    Fixed: previous commit

--- ruby/init-rhtml.el
+++ ruby/init-rhtml.el
@@ -3,7 +3,7 @@
 
 (autoload 'rhtml-mode "rhtml-mode" nil t)
 
-(add-to-list 'auto-mode-alist '("\\.erb\\" . rhtml-mode))
-(add-to-list 'auto-mode-alist '("\\.rjs\\" . rhtml-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . rhtml-mode))
+(add-to-list 'auto-mode-alist '("\\.rjs\\'" . rhtml-mode))
 
 (provide 'init-rhtml)
