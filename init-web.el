c3e27ceb653a28e931d51f91e1d2afb23fae8eee
Sviridov Alexander <sviridov.vmi@gmail.com>
2013-09-25 18:42:40 +0600

    Added scss-mode

--- init-web.el
+++ init-web.el
@@ -5,5 +5,6 @@
 (require 'init-closure-template-html-mode)
 (require 'init-yaml-mode)
 (require 'init-haml-mode)
+(require 'init-css)
 
 (provide 'init-web)
---
+++ web/init-css.el
@@ -0,0 +1,10 @@
+
+(add-load-path "web/scss-mode")
+
+(setq css-indent-offset 2)
+
+(autoload 'scss-mode "scss-mode" "Major mode for SCSS writing" t)
+
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+
+(provide 'init-css)
