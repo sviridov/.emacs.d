50b2f501e6f7095223eacf64fd5c71d284126d6e
Sviridov Alexander <sviridov.vmi@gmail.com>
2013-11-15 17:36:34 +0600

    Changed js2-mode indentation

--- javascript/init-js2-mode.el
+++ javascript/init-js2-mode.el
@@ -5,4 +5,7 @@
 
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
+(add-hook 'js2-mode-hook  #'(lambda () (setf js2-basic-offset 2
+                                             js2-bounce-indent-p t)))
+
 (provide 'init-js2-mode)
