22a4d25a8b4797aa51d0a0fb4896f7026cfc3dfc
Sviridov Alexander <sviridov.vmi@gmail.com>
2014-01-16 23:46:39 +0600

    Changed: popup.el colors

--- base/init-popup.el
+++ base/init-popup.el
@@ -1,9 +1,4 @@
 
 (require-package 'popup)
 
-(eval-after-load "popup"
- '(progn
-    (set-face-attribute 'popup-scroll-bar-background-face nil :background "#073642")
-    (set-face-attribute 'popup-scroll-bar-foreground-face nil :background "#073642" :box '(:line-width 1 :style none))))
-
 (provide 'init-popup)
--- visual/init-color-theme.el
+++ visual/init-color-theme.el
@@ -3,4 +3,11 @@
 
 (load-theme 'spacegray t)
 
+(eval-after-load "popup"
+ '(progn
+    (set-face-attribute 'popup-face     nil :foreground "#ffffff" :background "#1c1f26")
+    (set-face-attribute 'popup-tip-face nil :foreground "#ffffff" :background "#1c1f26")
+    (set-face-attribute 'popup-scroll-bar-background-face nil     :background "#1c1f26")
+    (set-face-attribute 'popup-scroll-bar-foreground-face nil     :background "#343d46")))
+
 (provide 'init-color-theme)
