37da8ac50d009c4870614e779049ccfbcb60485a
Sviridov Alexander <sviridov.vmi@gmail.com>
2014-01-16 22:17:04 +0600

    Updated: flycheck-check-syntax-automatically

--- code/init-flycheck.el
+++ code/init-flycheck.el
@@ -3,4 +3,8 @@
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
+(eval-after-load "flycheck"
+ '(setq flycheck-check-syntax-automatically '(save mode-enabled)
+        flycheck-idle-change-delay 0.8))
+
 (provide 'init-flycheck)
