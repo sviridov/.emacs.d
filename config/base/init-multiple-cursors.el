ace1f81ef4de07506a30e707202fda0f8302b75d
Sviridov Alexander <sviridov.vmi@gmail.com>
2014-02-09 13:56:08 +0600

    Removed: unnecessary autoloads for multiple-cursors

--- config/base/init-multiple-cursors.el
+++ config/base/init-multiple-cursors.el
@@ -1,12 +1,6 @@
 
 (require-package 'multiple-cursors)
 
-(dolist (function '(set-rectangular-region-anchor
-                    mc/mark-previous-like-this
-                    mc/mark-next-like-this
-                    mc/mark-all-like-this))
-  (autoload function "multiple-cursors" nil t))
-
 (global-set-key (kbd "S-SPC") 'set-rectangular-region-anchor)
 
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
