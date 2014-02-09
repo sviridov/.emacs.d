9111be7fd6666fc94da8b2cfa2ca684dce961ce0
Sviridov Alexander <sviridov.vmi@gmail.com>
2014-02-09 13:54:50 +0600

    Refactoring: config/base/init-dictem.el

--- config/base/init-dictem.el
+++ config/base/init-dictem.el
@@ -1,42 +1,53 @@
 
 (add-config-load-path "base/dictem")
 
-(dolist (function '(dictem-run-en-en
-                    dictem-run-en-ru
-                    dictem-run-ru-en
-                    dictem-run-ru-ru))
-  (autoload function "dictem" nil t))
+(--each '(dictem-run-en-en
+          dictem-run-en-ru
+          dictem-run-ru-en
+          dictem-run-ru-ru)
+  (autoload it "dictem" nil t))
+
+(defun dictem-ensure-buffer ()
+  (let* ((dictem-buffer (get-buffer-create dictem-buffer-name))
+         (dictem-window (get-buffer-window dictem-buffer))
+         (window-configuration (current-window-configuration))
+         (selected-window (frame-selected-window)))
+
+    (if (window-live-p dictem-window)
+        (select-window dictem-window)
+        (switch-to-buffer-other-window dictem-buffer))
+
+    (when (dictem-mode-p)
+      (if dictem-use-content-history
+          (setq dictem-content-history
+                (cons (list (buffer-substring (point-min) (point-max))
+                            (point))
+                      dictem-content-history)))
+      (setq buffer-read-only nil)
+      (erase-buffer))
+
+    (unless (dictem-mode-p)
+      (dictem-mode)
+      (make-local-variable 'dictem-window-configuration)
+      (make-local-variable 'dictem-selected-window)
+      (make-local-variable 'dictem-content-history)
+      (setq dictem-window-configuration window-configuration)
+      (setq dictem-selected-window selected-window))))
+
+(defun dictem-run-define-at-point (from-to)
+  (interactive)
+  (let* ((default-word (thing-at-point 'symbol))
+         (selected-window (frame-selected-window))
+         (dictem-query (s-trim default-word)))
+
+    (unless (zerop (length dictem-query))
+      (dictem-run 'dictem-base-search from-to dictem-query ".")
+      (select-window selected-window))))
 
 (eval-after-load "dictem"
  '(progn
     (setq dictem-use-existing-buffer t
           dictem-use-user-databases-only t)
-
-    (defun dictem-ensure-buffer ()
-      (let* ((dictem-buffer (get-buffer-create dictem-buffer-name))
-             (dictem-window (get-buffer-window dictem-buffer))
-             (window-configuration (current-window-configuration))
-             (selected-window (frame-selected-window)))
-        (if (window-live-p dictem-window)
-            (select-window dictem-window)
-          (switch-to-buffer-other-window dictem-buffer))
-
-        (if (dictem-mode-p)
-            (progn
-                  (if dictem-use-content-history
-                      (setq dictem-content-history
-                            (cons (list (buffer-substring
-                                         (point-min) (point-max))
-                                        (point)) dictem-content-history)))
-                  (setq buffer-read-only nil)
-                  (erase-buffer))
-          (progn
-            (dictem-mode)
-            (make-local-variable 'dictem-window-configuration)
-            (make-local-variable 'dictem-selected-window)
-            (make-local-variable 'dictem-content-history)
-            (setq dictem-window-configuration window-configuration)
-            (setq dictem-selected-window selected-window)))))
 
     (setq dictem-server "dict.mova.org"
           dictem-exclude-databases '("ger-" "-ger" "fra-" "-fra"))
@@ -60,25 +71,10 @@
       '(("_en-ru"  . ("mueller7" "korolew_en-ru" "en-ru"))
         ("_en-en"  . ("foldoc"))
         ("_ru-ru"  . ("beslov" "ushakov" "ozhegov" "brok_and_efr"))
-        ("_ru-en" . ("ru-en"))))
+        ("_ru-en"  . ("ru-en"))))
 
     (define-key dictem-mode-map [tab] 'dictem-next-link)
     (define-key dictem-mode-map [(backtab)] 'dictem-previous-link)
-
-    (defun dictem-run-define-at-point (from-to)
-      (interactive)
-      (let* ((default-word (thing-at-point 'symbol))
-             (selected-window (frame-selected-window))
-             (dictem-query
-              (funcall #'(lambda (str)
-                           "Remove Whitespace from beginning and end of a string."
-                           (replace-regexp-in-string "^[ \n\t]*\\(.*?\\)[ \n\t]*$"
-                                                     "\\1"
-                                                     str))
-                       default-word)))
-        (unless (= (length dictem-query) 0)
-          (dictem-run 'dictem-base-search from-to dictem-query ".")
-          (select-window selected-window))))
 
     (defun dictem-run-ru-en ()
       (interactive)
