0f3cddac5b1cdf913974155455d0f170bcbac94d
Sviridov Alexander <sviridov.vmi@gmail.com>
2014-02-21 17:29:35 +0600

    Refacoring: config/code/init-yasnippet.el

--- config/code/init-yasnippet.el
+++ config/code/init-yasnippet.el
@@ -5,19 +5,18 @@
  '(setq yas-prompt-functions '(yas-ido-prompt)
         yas-snippet-dirs (list (in-user-emacs-directory "config/code/snippets"))))
 
-(dolist (mode '("c-mode"
-                "c++-mode"
-                "rhtml-mode"
-                "ruby-mode"
-                "slime-repl-mode"
-                ))
-  (eval-after-load mode
-    (add-hook (intern (concat mode "-hook"))
-              (lambda () (yas/global-mode 1)))))
+(--each '("c-mode"
+          "c++-mode"
+          "rhtml-mode"
+          "ruby-mode"
+          "slime-repl-mode")
+  (eval-after-load it
+    (add-hook (intern (concat it "-hook"))
+              (lambda () (yas-global-mode 1)))))
 
 (add-hook 'after-init-hook
   (lambda ()
     (add-hook 'lisp-mode-hook
-      (lambda () (yas/global-mode 1)))))
+      (lambda () (yas-global-mode 1)))))
 
 (provide 'init-yasnippet)
