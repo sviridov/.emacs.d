36802acfb3d8c7c940ae394e52f7cb4d2f0db55c
Sviridov Alexander <sviridov.vmi@gmail.com>
2014-02-23 20:26:24 +0600

    Refacoring: config/elisp/init-elisp-highlight.el

--- config/elisp/init-elisp-highlight.el
+++ config/elisp/init-elisp-highlight.el
@@ -1,39 +1,37 @@
 
-(defmacro define-emacs-lisp-symbols-highlighter (name selector)
+(defmacro define-emacs-lisp-symbols-highlighter (name args selector)
  `(defun ,name (limit)
     (loop
 
      (unless (re-search-forward "[^() \"\t\n',@]+" limit t)
        (return nil))
 
-     (when (funcall (lambda (symbol) (ignore-errors ,selector))
-                    (intern-soft (buffer-substring (match-beginning 0) (match-end 0))))
+     (when ((lambda ,args ,selector)
+            (intern-soft (buffer-substring (match-beginning 0) (match-end 0))))
        (set-match-data (list (match-beginning 0) (match-end 0)))
        (return t)))))
 
-(define-emacs-lisp-symbols-highlighter highlight-emacs-lisp-macro
+(define-emacs-lisp-symbols-highlighter highlight-emacs-lisp-macro (symbol)
   (eq 'macro (car-safe (symbol-function symbol))))
 
-(define-emacs-lisp-symbols-highlighter highlight-emacs-lisp-function
+(define-emacs-lisp-symbols-highlighter highlight-emacs-lisp-function (symbol)
   (eq 'lambda (car-safe (symbol-function symbol))))
 
-(define-emacs-lisp-symbols-highlighter highlight-emacs-lisp-compiled-or-primitive-function
+(define-emacs-lisp-symbols-highlighter highlight-emacs-lisp-compiled-or-primitive-function (symbol)
   (or (subrp (symbol-function symbol))
       (eq 'compiled-function (type-of (symbol-function symbol)))))
 
-;(define-emacs-lisp-symbols-highlighter highlight-emacs-lisp-global-variable
-;  (and (boundp symbol)
-;       (not (or (eq symbol t)
-;                (eq symbol nil)
-;                ))))
+(define-emacs-lisp-symbols-highlighter highlight-emacs-lisp-global-variable (symbol)
+  (and (boundp symbol)
+       (not (or (eq symbol t)
+                (eq symbol nil)))))
 
 (dolist (mode '(emacs-lisp-mode inferior-emacs-lisp-mode))
   (font-lock-add-keywords mode
     '((highlight-emacs-lisp-macro . 'font-lock-keyword-face)
       (highlight-emacs-lisp-function . 'font-lock-function-name-face)
       (highlight-emacs-lisp-compiled-or-primitive-function . 'font-lock-builtin-face)
-  ;   (highlight-emacs-lisp-global-variable . 'font-lock-variable-name-face)
-      )
+      (highlight-emacs-lisp-global-variable . 'font-lock-variable-name-face))
     t))
 
 (provide 'init-elisp-highlight)
