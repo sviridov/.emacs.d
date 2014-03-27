ab876e55e2ee0a27a7454996b7aa5f3cfddb8957
Sviridov Alexander <sviridov.vmi@gmail.com>
2014-03-27 13:01:21 +0600

    Added: company-cider

--- config/code/init-company.el
+++ config/code/init-company.el
@@ -1,7 +1,11 @@
 
 (require-package 'company)
 (require-package 'slime-company)
+(require-package 'company-cider)
 
 (add-hook 'after-init-hook 'global-company-mode)
+
+(eval-after-load "company"
+ '(add-to-list 'company-backends 'company-cider))
 
 (provide 'init-company)
