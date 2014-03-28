7ee1d584666f121fcf4f8c5f4c5a49fa0dae1c9e
Sviridov Alexander <sviridov.vmi@gmail.com>
2014-03-28 14:41:38 +0600

    Added: projectile-rails

--- config/base/init-projectile.el
+++ config/base/init-projectile.el
@@ -1,9 +1,12 @@
 
 (require-package 'projectile)
+(require-package 'projectile-rails)
 
 (setq projectile-known-projects-file "~/.projectile-bookmarks"
       projectile-switch-project-action 'projectile-dired)
 
 (projectile-global-mode)
+
+(add-hook 'projectile-mode-hook 'projectile-rails-on)
 
 (provide 'init-projectile)
