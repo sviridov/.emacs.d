9473816f4f2085585ec06b8cd3007297c2d7ec43
Sviridov Alexander <sviridov.vmi@gmail.com>
2014-03-31 20:41:26 +0600

    Added: advice for org-trello/install-board-and-lists-ids

--- config/base/init-org.el
+++ config/base/init-org.el
@@ -8,4 +8,8 @@
 
 (add-hook 'org-mode-hook 'org-trello-mode)
 
+(defadvice org-trello/install-board-and-lists-ids (around force-resize-mini-windows)
+  (let ((resize-mini-windows t))
+    ad-do-it))
+
 (provide 'init-org)
