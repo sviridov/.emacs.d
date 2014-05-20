ccfb6eee8ec6818968ae3ff974a8b65d2ec973d7
Sviridov Alexander <sviridov.vmi@gmail.com>
2014-05-20 13:02:25 +0600

    Init fonts when --daemon

--- config/visual/init-font.el
+++ config/visual/init-font.el
@@ -1,7 +1,5 @@
 
-(when (and (not (daemonp))
-           (find-font (font-spec :name "Ubuntu Mono-13")))
-  (set-frame-font "Ubuntu Mono-13" nil t)
-  (set-face-attribute 'mode-line nil :family "Ubuntu Mono"))
+(add-to-list 'default-frame-alist '(font . "Ubuntu Mono-13"))
+(set-face-attribute 'mode-line nil :family "Ubuntu Mono")
 
 (provide 'init-font)
