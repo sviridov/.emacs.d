ddaca536ae41d2f44551e1da30b020ea03b55894
Sviridov Alexander <sviridov.vmi@gmail.com>
2014-07-24 14:45:59 +0600

    Updated: diminish

--- config/base/init-diminish.el
+++ config/base/init-diminish.el
@@ -1,7 +1,7 @@
 
 (require-package 'diminish)
 
-(defmacro rename-modeline (package-name mode new-name)
+(defmacro rename-major-mode (package-name mode new-name)
  `(eval-after-load ,package-name
    '(defadvice ,mode (after rename-modeline activate)
       (setq mode-name ,new-name))))
@@ -10,11 +10,16 @@
  `(eval-after-load ,package
    '(diminish ',mode ,new-name)))
 
+(rename-major-mode "ruby-mode" ruby-mode "RUBY")
+
 (add-hook 'emacs-lisp-mode-hook (lambda() (setq mode-name "ELISP")))
 
 (rename-minor-mode "company" company-mode "CMP")
 (rename-minor-mode "eldoc" eldoc-mode "DOC")
 (rename-minor-mode "elisp-slime-nav" elisp-slime-nav-mode "NAV")
+(rename-minor-mode "magit" magit-auto-revert-mode nil)
+(rename-minor-mode "rubocop" rubocop-mode nil)
+(rename-minor-mode "yasnippet" yas-minor-mode " Y")
 
 (eval-after-load "flycheck"
  '(setq flycheck-mode-line-lighter " FC"))
