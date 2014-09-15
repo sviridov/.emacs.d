5c8868cc9b6eac0d922014af4da1f42ce70eb2d8
Sviridov Alexander <sviridov.vmi@gmail.com>
2014-09-15 13:22:09 +0600

    Removed: coffee auto-mode-alist

--- config/coffeescript/init-coffee-mode.el
+++ config/coffeescript/init-coffee-mode.el
@@ -1,11 +1,6 @@
 
 (require-package 'coffee-mode)
 
-(--each '("\\.coffee\\'"
-          "Cakefile\\'"
-          "coffee")
- (add-to-list 'auto-mode-alist `(,it . coffee-mode)))
-
 (setq coffee-tab-width 2)
 
 (provide 'init-coffee-mode)
