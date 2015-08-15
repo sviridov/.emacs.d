1b17d47f82bd161663cee311c8eef8539c419302
Sviridov Alexander <sviridov.vmi@gmail.com>
2015-08-15 17:02:28 +0500

    Removed: org-trello

--- config/base/init-org.el
+++ config/base/init-org.el
@@ -4,12 +4,4 @@
     (require 'org-protocol)
     (setq org-startup-indented t)))
 
-(require-package 'org-trello)
-
-(add-hook 'org-mode-hook 'org-trello-mode)
-
-(defadvice org-trello/install-board-and-lists-ids (around force-resize-mini-windows)
-  (let ((resize-mini-windows t))
-    ad-do-it))
-
 (provide 'init-org)
