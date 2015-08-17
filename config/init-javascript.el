6501eaae691d094133ad143062631847eac36b7e
Sviridov Alexander <sviridov.vmi@gmail.com>
2015-08-17 08:39:25 +0500

    Changed: default js-indent-level

--- config/init-javascript.el
+++ config/init-javascript.el
@@ -1,6 +1,8 @@
 
 (add-config-load-path "javascript")
 
+(add-hook 'js-mode-hook (lambda () (setq js-indent-level 2)))
+
 (require 'init-js2-mode)
 (require 'init-js2-refactor)
 (require 'init-skewer-mode)
