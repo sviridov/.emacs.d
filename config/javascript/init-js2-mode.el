2c7d57e8e02ddb19116c36deaa7f7d658efe8370
Sviridov Alexander <sviridov.vmi@gmail.com>
2015-09-16 12:31:49 +0500

    Improved: JS2-mode config

--- config/javascript/init-js2-mode.el
+++ config/javascript/init-js2-mode.el
@@ -3,6 +3,16 @@
 
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
-(setq-default js2-basic-offset 2)
+(setq-default js2-basic-offset 2
+              js2-bounce-indent-p t)
+
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (electric-indent-mode -1)
+
+            (define-key js2-mode-map (kbd "M-j")
+              (lambda ()
+                (interactive)
+                (join-line -1)))))
 
 (provide 'init-js2-mode)
