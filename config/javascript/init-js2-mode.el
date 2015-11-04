ec4608c912302e20a89dcb385e54e3e95406a099
Sviridov Alexander <sviridov.vmi@gmail.com>
2015-11-04 15:35:04 +0500

    Updated: js2-mode config according to airbnb style guide

--- config/javascript/init-js2-mode.el
+++ config/javascript/init-js2-mode.el
@@ -2,9 +2,12 @@
 (require-package 'js2-mode)
 
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
 
 (setq-default js2-basic-offset 2
-              js2-bounce-indent-p t)
+              js2-bounce-indent-p t
+              js2-strict-missing-semi-warning nil
+              js2-strict-trailing-comma-warning nil)
 
 (add-hook 'js2-mode-hook
           (lambda ()
