03ab61cbe0d80ba86cc0c5601a1eeaebfefc063d
Sviridov Alexander <sviridov.vmi@gmail.com>
2015-11-08 16:21:41 +0500

    Turn off js2-strict-inconsistent-return-warning

--- config/javascript/init-js2-mode.el
+++ config/javascript/init-js2-mode.el
@@ -7,7 +7,8 @@
 (setq-default js2-basic-offset 2
               js2-bounce-indent-p t
               js2-strict-missing-semi-warning nil
-              js2-strict-trailing-comma-warning nil)
+              js2-strict-trailing-comma-warning nil
+              js2-strict-inconsistent-return-warning nil)
 
 (add-hook 'js2-mode-hook
           (lambda ()
